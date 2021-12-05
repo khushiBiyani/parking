961d49dcc277bb92f69cb09d81a602bd6b830bbb
Kishore <lnemalipuri@salesforce.com>
2021-12-05 22:59:46 +0530

    Adding feedback rating
    Signed-off-by: Kishore <lnemalipuri@salesforce.com>

--- src/components/role-type-dashboards/services.js
+++ src/components/role-type-dashboards/services.js
@@ -5,8 +5,33 @@ import { useAuth } from "../../contexts/AuthContext"
 import { PermIdentity, DirectionsCar } from "@material-ui/icons";
 import {useLocation} from "react-router-dom";
 import { useHistory } from "react-router-dom"
-
+import rating from './rating'
 import { Button } from "react-bootstrap"
+import { makeStyles } from "@material-ui/core/styles";
+import Rating from "@material-ui/lab/Rating";
+
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+    display: "flex",
+    alignItems: "center"
+  },
+  iconFilled: {},
+  iconFilled1: { color: "red" },
+  iconFilled2: { color: "orange" },
+  iconFilled3: { color: "yellow" },
+  iconFilled4: { color: "blue" },
+  iconFilled5: { color: "green" },
+  iconHover: {},
+  iconHover1: { color: "red" },
+  iconHover2: { color: "orange" },
+  iconHover3: { color: "yellow" },
+  iconHover4: { color: "blue" },
+  iconHover5: { color: "green" }
+});
+
 
 export default function Services() {
     const history = useHistory()
@@ -14,6 +39,13 @@ export default function Services() {
     const checkinTime = data?.state?.checkinTime;
     const checkoutTime = data?.state?.checkoutTime;
 
+
+    const classes = useStyles();
+    const [value, setValue] = useState(2);
+    const [iconFilledVar, setIconFilled] = useState(classes.iconFilled);
+    const [iconHoverVar, setIconHover] = useState(classes.iconHover);
+
+    
     const totalTime = (checkinTime && checkoutTime && (checkoutTime - checkinTime) > 0 )? (checkoutTime - checkinTime)  : 1;
     const [loading, setLoading] = useState(false);
     const [worker, setWorker] = useState([]);
@@ -153,10 +185,82 @@ export default function Services() {
                               </div>
                               </div>
                           </div>
-                          <Link to="/ratings" className="link">
+                          {/* <Link to="/ratings" className="link">
                               <PermIdentity className="sidebarIcon" />
                               Ratings
+                          </Link> */}
+
+                          <div>
+                            Please rate your experience:
+                            <div className={classes.root}>
+                              <Rating
+                                name="hover-feedback"
+                                value={value}
+                                precision={1}
+                                onChange={(event, newValue) => {
+                                  console.log(event, "This is th e event");
+                                  setValue(newValue);
+                                  switch (true) {
+                                    case newValue <= 1: {
+                                      setIconFilled(classes.iconFilled1);
+                                      break;
+                                    }
+                                    case newValue <= 2 && newValue > 1: {
+                                      setIconFilled(classes.iconFilled2);
+                                      break;
+                                    }
+                                    case newValue <= 3 && newValue > 2: {
+                                      setIconFilled(classes.iconFilled3);
+                                      break;
+                                    }
+                                    case newValue <= 4 && newValue > 3: {
+                                      setIconFilled(classes.iconFilled4);
+                                      break;
+                                    }
+                                    case newValue > 4: {
+                                      setIconFilled(classes.iconFilled5);
+                                      break;
+                                    }
+                                  }
+                                }}
+                                onChangeActive={(event, newHover) => {
+                                  switch (true) {
+                                    case newHover <= 1: {
+                                      setIconHover(classes.iconHover1);
+                                      break;
+                                    }
+                                    case newHover <= 2 && newHover > 1: {
+                                      setIconHover(classes.iconHover2);
+                                      break;
+                                    }
+                                    case newHover <= 3 && newHover > 2: {
+                                      setIconHover(classes.iconHover3);
+                                      break;
+                                    }
+                                    case newHover <= 4 && newHover > 3: {
+                                      setIconHover(classes.iconHover4);
+                                      break;
+                                    }
+                                    case newHover > 4: {
+                                      setIconHover(classes.iconHover5);
+                                      break;
+                                    }
+                                  }
+                                }}
+                                defaultValue={2}
+                                icon={<FiberManualRecordIcon fontSize="inherit" />}
+                                classes={{
+                                  iconFilled: iconFilledVar,
+                                  iconHover: iconHoverVar
+                                }}
+                              />
+                            </div>
+                          </div>
+                          <Link to="/" className="link">
+                              <PermIdentity className="sidebarIcon" />
+                              Go Home
                           </Link>
+
                       </div>
                     )}
                     

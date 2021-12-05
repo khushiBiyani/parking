9139ad8631444233c8db9f4104d33d8d262a601c
khushiBiyani <93194800+khushiBiyani@users.noreply.github.com>
2021-12-05 13:03:13 +0530

    comment eveyrthing in services.js before running

---
+++ src/components/role-type-dashboards/services.js
@@ -0,0 +1,13 @@
+// import React, { useState, useEffect } from "react";
+// import firebase from "../../firebase";
+// import { Link } from "react-router-dom";
+// import { useAuth } from "../../contexts/AuthContext"
+// import { PermIdentity, Storefront, DirectionsCar } from "@material-ui/icons";
+
+// export default function UserDashboard() {
+//     const [loading, setLoading] = useState(false);
+//     const [worker, setWorker] = useState([]);
+//     const [size, setSize] = useState([]);
+//     const [userSelectedLocation, setUserLocation] = useState([]);
+//     const [textInput, setTextInput] = useState([]);
+//     const { currentUser } = useAuth();
